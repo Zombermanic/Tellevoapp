46f2f9a4fab97838b5e5978ff043537a408d28e2
AlexanderTM19 <davi.salas@duocuc.cl>
2023-11-12 18:34:03 -0300

    Se agrega pagina conductor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,33 +6,40 @@ import { IngresarGuard } from './ingresar.guard';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
     canActivate: [NoIngresadoGuard]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'conductor',
     pathMatch: 'full'
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule),
+    loadChildren: () => import('./inicio/inicio.module').then(m => m.InicioPageModule),
     canActivate: [IngresarGuard]
   },
   {
     path: 'recuperar',
-    loadChildren: () => import('./recuperar/recuperar.module').then( m => m.RecuperarPageModule),
+    loadChildren: () => import('./recuperar/recuperar.module').then(m => m.RecuperarPageModule),
     canActivate: [NoIngresadoGuard]
   },
   {
     path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
+    loadChildren: () => import('./registro/registro.module').then(m => m.RegistroPageModule),
     canActivate: [NoIngresadoGuard]
+  }, 
+  {
+    path: 'conductor',
+    loadChildren: () => import('./conductor/conductor.module').then(m => m.ConductorPageModule)
   },
   {
     path: '**',
-    loadChildren: () => import('./error/error.module').then( m => m.ErrorPageModule)
+    loadChildren: () => import('./error/error.module').then(m => m.ErrorPageModule)
   },
+
+
+
 
 ];
 
